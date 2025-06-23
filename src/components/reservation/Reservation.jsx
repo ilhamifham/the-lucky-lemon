861734c7bb33725422def39bac10a530b4a94089
Ilham Ifham <ilhaamifham@gmail.com>
2025-06-23 20:01:35 +0530

    Updated the form design

--- src/components/reservation/Reservation.jsx
+++ src/components/reservation/Reservation.jsx
@@ -2,26 +2,113 @@ import "./Reservation.css";
 import Chevron from "../ui/Chevron.jsx";
 
 function Reservation() {
+    const date = new Date();
+    const daysInMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+
     return (
-        <form>
+        <form onSubmit={(event) => handleFormSubmit(event)}>
             <label htmlFor="name">Full name</label>
-            <input type="text" name="name" id="name" placeholder="Enter your full name" />
+            <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter your full name"
+            />
             <label htmlFor="phone">Phone number</label>
-            <input type="tel" name="phone" id="phone" placeholder="Enter your phone number" />
-            <label htmlFor="phone">Email</label>
-            <input type="email" name="email" id="email" placeholder="Enter your email" />
-            <div>
-                <div>
-                    <label htmlFor="date">Date to come</label>
-                    <input type="date" name="date" id="date" />
+            <input
+                type="tel"
+                name="phone"
+                id="phone"
+                placeholder="Enter your phone number"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+            />
+            <div className="label">Date to come</div>
+            <div className="date">
+                <div className="select-group">
+                    <select name="day">
+                        {Array.from({ length: daysInMonth }, (_, index) => index + 1).map(
+                            (day) => {
+                                return (
+                                    <option key={day} value={day}>
+                                        {day}
+                                    </option>
+                                );
+                            }
+                        )}
+                    </select>
+                    <Chevron />
                 </div>
-                <div>
-                    <label htmlFor="time">Time to come</label>
-                    <input type="time" name="time" id="time" />
+                <div className="select-group">
+                    <select name="month">
+                        {Array.from({ length: 12 }, (_, index) => index + 1).map(
+                            (month) => {
+                                return (
+                                    <option key={month} value={month}>
+                                        {month}
+                                    </option>
+                                );
+                            }
+                        )}
+                    </select>
+                    <Chevron />
+                </div>
+                <div className="select-group">
+                    <select name="month">
+                        {Array.from(
+                            { length: 2 },
+                            (_, index) => date.getFullYear() + index
+                        ).map((year) => {
+                            return (
+                                <option key={year} value={year}>
+                                    {year}
+                                </option>
+                            );
+                        })}
+                    </select>
+                    <Chevron />
                 </div>
             </div>
+            <label htmlFor="time">Time to come</label>
+            <div className="select-group">
+                <select name="time" id="time">
+                    <option value="">08 : 00 A.M</option>
+                    <option value="">09 : 15 A.M</option>
+                    <option value="">10 : 30 A.M</option>
+                    <option value="">11 : 45 A.M</option>
+                    <option value="">01 : 00 P.M</option>
+                    <option value="">02 : 15 P.M</option>
+                    <option value="">03 : 30 P.M</option>
+                    <option value="">04 : 45 P.M</option>
+                    <option value="">06 : 00 P.M</option>
+                    <option value="">07 : 15 P.M</option>
+                    <option value="">08 : 30 P.M</option>
+                    <option value="">09 : 45 P.M</option>
+                </select>
+                <Chevron />
+            </div>
             <label htmlFor="people">Number of people</label>
-            <input type="number" name="people" id="people" placeholder="Enter number of people attending" />
+            <div className="select-group">
+                <select name="people" id="people">
+                    {Array.from({ length: 12 }, (_, index) => index + 1).map((people) => {
+                        return (
+                            <option key={people} value={people}>
+                                {people}
+                            </option>
+                        );
+                    })}
+                </select>
+                <Chevron />
+            </div>
             <label htmlFor="occasions">Occasions</label>
             <div className="select-group">
                 <select name="occasions" id="occasions">
@@ -34,7 +121,7 @@ function Reservation() {
             </div>
             <button className="button__ui">Reserve a Table</button>
         </form>
-    )
+    );
 }
 
 export default Reservation;
