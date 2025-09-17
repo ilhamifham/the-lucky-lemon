df3157f7c1e187dc94522a7426d0b4bf91a297ca
Ilham Ifham <ilhaamifham@gmail.com>
2025-09-17 13:44:53 +0530

    updated the context provider

--- src/components/provider/CartContextProvider.jsx
+++ src/components/provider/CartContextProvider.jsx
@@ -2,16 +2,44 @@ import { useState, useEffect, useMemo } from "react";
 import { CartContext } from "/src/contexts/useCartContext.js";
 
 function CartContextProvider({ children }) {
-  const [cartItems, setCartItems] = useState(() => JSON.parse(localStorage.getItem("cartItems")) || []);
-  const cartItemsCount = useMemo(() => cartItems.reduce((total, currentCartItem) => total + currentCartItem.quantity, 0), [cartItems]);
-  const cartItemsPrice = useMemo(() => cartItems.reduce((total, currentCartItem) => total + currentCartItem.price * currentCartItem.quantity, 0), [cartItems]);
+  const [cartItems, setCartItems] = useState(
+    () => JSON.parse(localStorage.getItem("cartItems")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const cartItemsCount = useMemo(
+    () =>
+      cartItems.reduce(
+        (total, currentCartItem) => total + currentCartItem.quantity,
+        0
+      ),
+    [cartItems]
+  );
+  const cartItemsPrice = useMemo(
+    () =>
+      cartItems.reduce(
+        (total, currentCartItem) =>
+          total + currentCartItem.price * currentCartItem.quantity,
+        0
+      ),
+    [cartItems]
+  );
 
   function addToCart(item) {
     setCartItems((prevCartItems) => {
-      const existingCartItem = prevCartItems.find((prevCartItem) => prevCartItem.title === item.title);
+      const existingCartItem = prevCartItems.find(
+        (prevCartItem) => prevCartItem.title === item.title
+      );
 
       if (existingCartItem) {
-        return prevCartItems.map((prevCartItem) => (prevCartItem.title === item.title ? { ...prevCartItem, quantity: prevCartItem.quantity + 1 } : prevCartItem));
+        return prevCartItems.map((prevCartItem) =>
+          prevCartItem.title === item.title
+            ? { ...prevCartItem, quantity: prevCartItem.quantity + 1 }
+            : prevCartItem
+        );
       }
 
       return [...prevCartItems, { ...item, quantity: 1 }];
@@ -20,11 +48,17 @@ function CartContextProvider({ children }) {
 
   function removeFromCart(item) {
     setCartItems((prevCartItems) => {
-      const existingCartItem = prevCartItems.find((prevCartItem) => prevCartItem.title === item.title);
+      const existingCartItem = prevCartItems.find(
+        (prevCartItem) => prevCartItem.title === item.title
+      );
 
       if (existingCartItem) {
         return prevCartItems
-          .map((prevCartItem) => (prevCartItem.title === item.title ? { ...prevCartItem, quantity: prevCartItem.quantity - 1 } : prevCartItem))
+          .map((prevCartItem) =>
+            prevCartItem.title === item.title
+              ? { ...prevCartItem, quantity: prevCartItem.quantity - 1 }
+              : prevCartItem
+          )
           .filter((prevCartItem) => prevCartItem.quantity > 0);
       }
 
@@ -34,10 +68,16 @@ function CartContextProvider({ children }) {
 
   function changeQuantity(item, quantity) {
     setCartItems((prevCartItems) => {
-      const existingCartItem = prevCartItems.find((prevCartItem) => prevCartItem.title === item.title);
+      const existingCartItem = prevCartItems.find(
+        (prevCartItem) => prevCartItem.title === item.title
+      );
 
       if (existingCartItem) {
-        return prevCartItems.map((prevCartItem) => (prevCartItem.title === item.title ? { ...prevCartItem, quantity: parseInt(quantity) } : prevCartItem));
+        return prevCartItems.map((prevCartItem) =>
+          prevCartItem.title === item.title
+            ? { ...prevCzartItem, quantity: parseInt(quantity) }
+            : prevCartItem
+        );
       }
 
       return prevCartItems;
@@ -46,15 +86,28 @@ function CartContextProvider({ children }) {
 
   function deleteCartItem(item) {
     setCartItems((prevCartItems) => {
-      return prevCartItems.filter((prevCartItem) => prevCartItem.title !== item.title);
+      return prevCartItems.filter(
+        (prevCartItem) => prevCartItem.title !== item.title
+      );
     });
   }
 
-  useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
-
-  return <CartContext.Provider value={{ cartItems, setCartItems, cartItemsCount, cartItemsPrice, addToCart, removeFromCart, changeQuantity, deleteCartItem }}>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        setCartItems,
+        cartItemsCount,
+        cartItemsPrice,
+        addToCart,
+        removeFromCart,
+        changeQuantity,
+        deleteCartItem,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export default CartContextProvider;
